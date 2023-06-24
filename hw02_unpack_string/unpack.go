ee81d37e2d7d546ac10d54aec524d3ee888dd7a7
Алиев Турал Камил Оглы <tkaliev@beeline.ru>
2023-06-24 20:19:13 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,54 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(InputWord string) (string, error) {
+	var result strings.Builder
+	inputWordRune := []rune(InputWord)
+	if len(inputWordRune) == 0 {
+		return "", nil
+	}
+	preValue := ""
+	_, err := strconv.Atoi(string(inputWordRune[0]))
+	if err == nil {
+		return "", ErrInvalidString
+	}
+	for i, s := range inputWordRune {
+		curr, err := strconv.Atoi(string(s))
+		if err != nil {
+			if i == len(inputWordRune)-1 {
+				_, err := strconv.Atoi(preValue)
+				if err != nil {
+					result.WriteString(preValue)
+				}
+				result.WriteString(string(s))
+
+			} else {
+				_, err := strconv.Atoi(preValue)
+				if err == nil {
+					preValue = string(s)
+				} else {
+					result.WriteString(preValue)
+					preValue = string(s)
+				}
+
+			}
+		} else {
+			_, err := strconv.Atoi(preValue)
+			if err != nil {
+				result.WriteString(strings.Repeat(preValue, curr))
+				preValue = string(s)
+			} else {
+				return "", ErrInvalidString
+			}
+		}
+
+	}
+
+	return result.String(), nil
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -16,6 +16,9 @@ func TestUnpack(t *testing.T) {
 		{input: "abccd", expected: "abccd"},
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
+		{input: "a1", expected: "a"},
+		{input: "a0", expected: ""},
+		{input: "*5", expected: "*****"},
 		// uncomment if task with asterisk completed
 		// {input: `qwe\4\5`, expected: `qwe45`},
 		// {input: `qwe\45`, expected: `qwe44444`},
