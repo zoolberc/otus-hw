5e1cff8d13951ef533a7f699d7a7c99f7e7f80bf
Алиев Турал Камил Оглы <tkaliev@beeline.ru>
2023-07-22 17:01:28 +0300

    HW3 update

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,17 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"golang.org/x/example/stringutil"
+)
 
 func main() {
-	//reversWord := stringutil.Reverse("Hello, OTUS!")
-	//fmt.Println(reversWord)
-	wordsMap := make(map[string]int)
-
-	for i := 0; i < 10; i++ {
-		wordsMap[string(rune(i))]++
-	}
-
-	for i := range wordsMap {
-		fmt.Printf("Key = %s, value = %d\n", i, wordsMap[i])
-	}
+	reversWord := stringutil.Reverse("Hello, OTUS!")
+	fmt.Println(reversWord)
 }
