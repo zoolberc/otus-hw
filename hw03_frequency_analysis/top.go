a919e627d251ff65d72e48bf6fe130a424b70291
Алиев Турал Камил Оглы <tkaliev@beeline.ru>
2023-07-22 16:01:23 +0300

    HW3 update

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -17,31 +17,43 @@ func Top10(inputText string) []string {
 		wordToCountMap[word]++
 	}
 
-	countToWordsMap := make(map[int][]string)
-	for word, count := range wordToCountMap {
-		words := countToWordsMap[count]
-		words = append(words, word)
-		countToWordsMap[count] = words
-	}
-
-	counts := make([]int, 0, len(countToWordsMap))
-	for count := range countToWordsMap {
-		counts = append(counts, count)
-	}
-	sort.Ints(counts)
-
-	result := make([]string, 0, 10)
-	for i := len(counts) - 1; i >= 0 && len(result) < 10; i-- {
-		words := countToWordsMap[counts[i]]
-		sort.Strings(words)
-		for _, word := range words {
-			if len(result) < 10 {
-				result = append(result, word)
-			} else {
-				break
-			}
+	sort.Slice(allWords, func(i, j int) bool {
+		if wordToCountMap[allWords[i]] == wordToCountMap[allWords[j]] {
+			return allWords[i] < allWords[j]
 		}
-	}
+		return wordToCountMap[allWords[i]] > wordToCountMap[allWords[j]]
+	})
 
-	return result
+	max := 10
+	if l := len(allWords); l < max {
+		max = l
+	}
+	return allWords[:max]
+	//countToWordsMap := make(map[int][]string)
+	//for word, count := range wordToCountMap {
+	//	words := countToWordsMap[count]
+	//	words = append(words, word)
+	//	countToWordsMap[count] = words
+	//}
+	//
+	//counts := make([]int, 0, len(countToWordsMap))
+	//for count := range countToWordsMap {
+	//	counts = append(counts, count)
+	//}
+	//sort.Ints(counts)
+	//
+	//result := make([]string, 0, 10)
+	//for i := len(counts) - 1; i >= 0 && len(result) < 10; i-- {
+	//	words := countToWordsMap[counts[i]]
+	//	sort.Strings(words)
+	//	for _, word := range words {
+	//		if len(result) < 10 {
+	//			result = append(result, word)
+	//		} else {
+	//			break
+	//		}
+	//	}
+	//}
+
+	//return result
 }
