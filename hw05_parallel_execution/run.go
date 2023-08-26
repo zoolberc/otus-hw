78ab2af77ed7a7b6d7531474deff0734d27d1c3f
Алиев Турал Камил Оглы <tkaliev@beeline.ru>
2023-08-26 17:35:26 +0300

    hw05 completed

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,14 +2,49 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
+	"sync/atomic"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
-// Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
-func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+func Run(tasks []Task, goroutinesCount, maxErrors int) error {
+	if maxErrors <= 0 {
+		return ErrErrorsLimitExceeded
+	}
+	taskChan := make(chan Task, len(tasks))
+
+	wg := &sync.WaitGroup{}
+	wg.Add(goroutinesCount)
+
+	for _, t := range tasks {
+		taskChan <- t
+	}
+	close(taskChan)
+	var errorsCount int32
+
+	for i := 0; i < goroutinesCount; i++ {
+		go worker(&errorsCount, int32(maxErrors), wg, taskChan)
+	}
+	wg.Wait()
+	if errorsCount > int32(maxErrors) {
+		return ErrErrorsLimitExceeded
+	}
 	return nil
+}
+
+func worker(errorsCount *int32, maxErrors int32, wg *sync.WaitGroup, taskChan chan Task) {
+	defer wg.Done()
+	for atomic.LoadInt32(errorsCount) < maxErrors {
+		task, ok := <-taskChan
+		if !ok {
+			return
+		}
+		err := task()
+		if err != nil {
+			atomic.AddInt32(errorsCount, 1)
+		}
+	}
 }
--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -67,4 +67,27 @@ func TestRun(t *testing.T) {
 		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
 		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
 	})
+
+	t.Run("Getting an error at the maximum count of errors is 0", func(t *testing.T) {
+		tasksCount := 50
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+
+		for i := 0; i < tasksCount; i++ {
+			taskSleep := time.Millisecond * time.Duration(rand.Intn(100))
+
+			tasks = append(tasks, func() error {
+				time.Sleep(taskSleep)
+				atomic.AddInt32(&runTasksCount, 1)
+				return nil
+			})
+		}
+
+		workersCount := 5
+		maxErrorsCount := 0
+		err := Run(tasks, workersCount, maxErrorsCount)
+
+		require.Truef(t, errors.Is(err, ErrErrorsLimitExceeded), "actual err - %v", err)
+	})
 }
